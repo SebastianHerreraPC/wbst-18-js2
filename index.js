4d97aa052bbae2105763ab74bc8fdf5942f13e30
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-11 23:10:25 -0600

    operadores

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+console.log("hola mundo");
 
+// comparadores y operadores
+
+console.log(7 > 6); //mayor que
+console.log(7 < 6); //menor que
+console.log(7 >= 6); //mayor o igual a
+console.log(7 <= 6); //menor o igual a
+console.log(70 == "70"); // igualdad
+console.log(70 != 70); //desigualdad
+console.log(70 === "70"); //igualdad estricta
+console.log(70 !== 70); //desigualdad estricta
