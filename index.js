740bf4b230bd700733aff03d5c37ac08fab3c813
SebastianHerreraPc <sebehepe00@gmail.com>
2024-06-12 22:27:35 -0600

    sentencia condicional

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ console.log(7 < 6); //menor que
 console.log(7 >= 6); //mayor o igual a
 console.log(7 <= 6); //menor o igual a
 console.log(70 == "70"); // igualdad
-console.log(70 != 70); //desigualdad
+console.log(70 != 60); //desigualdad
 console.log(70 === "70"); //igualdad estricta
 console.log(70 !== 70); //desigualdad estricta
+
+//sentencias condicionales:
+
+let lavariable = true;
+
+if (lavariable) {
+  console.log("😬");
+}
